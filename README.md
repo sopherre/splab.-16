965ea4257a50eacd2fc980bfbf6d358b789736fb
Shun E <shun-eto@sopherre.com>
2023-08-21 14:03:10 +0900

    修正

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # splab.-16
+
+## スクリプトの実行
+
+```sh
+./node_modules/.bin/token-transformer data/tokens.json output.json
+```
+
+```sh
+pnpm style-dictionary build --config style-dictionary.config.json
+```
+
+==> src/tokens/data.ts にトークン情報が出力される
--- src/tokens/tokens.ts
+++ src/tokens/tokens.ts
@@ -1,30 +1,56 @@
 /**
  * Do not edit directly
- * Generated on Sat, 19 Aug 2023 06:16:58 GMT
+ * Generated on Mon, 21 Aug 2023 05:00:19 GMT
  */
 
-export const H1 = {
-  fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-  fontWeight: 500,
-  lineHeight: 32,
-  fontSize: 28,
-  letterSpacing: 0,
-};
-export const H2 = {
-  fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-  fontWeight: 400,
-  fontSize: 24,
-  letterSpacing: 0,
-  lineHeight: 24,
-};
-export const H3 = {
-  fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-  fontWeight: 400,
-  lineHeight: 20,
-  fontSize: 20,
-  letterSpacing: 0,
-};
-export const PalettePrimaryMain = "#F5F5F5";
-export const SecondaryMain = "#2196F3";
-export const BackgroundDefault = "#FFFFFF";
-export const TextSecondary = "#37352fa6";
+export const PalettePrimary50 = "#FAFAFA";
+export const PalettePrimary100 = "#F5F5F5";
+export const PalettePrimary200 = "#EEEEEE";
+export const PalettePrimary300 = "#E0E0E0";
+export const PalettePrimary400 = "#BDBDBD";
+export const PalettePrimary500 = "#9E9E9E";
+export const PalettePrimary600 = "#757575";
+export const PalettePrimary700 = "#616161";
+export const PalettePrimary800 = "#424242";
+export const PalettePrimary900 = "#212121";
+export const PalettePrimaryMain = "#FFFFFF";
+export const PaletteSecondary50 = "#2196f30a";
+export const PaletteSecondary100 = "#BBDEFB";
+export const PaletteSecondary200 = "#2196f380";
+export const PaletteSecondary300 = "#64B5F6";
+export const PaletteSecondary400 = "#42A5F5";
+export const PaletteSecondary500 = "#2196F3";
+export const PaletteSecondary600 = "#1E88E5";
+export const PaletteSecondary700 = "#1976D2";
+export const PaletteSecondary800 = "#1565C0";
+export const PaletteSecondary900 = "#0D47A1";
+export const PaletteSecondaryMain = "#2196F3";
+export const PaletteErrorMain = "#D32F2F";
+export const PaletteTextPrimary = "#000000de";
+export const PaletteTextSecondary = "#37352fa6";
+export const PaletteTextDisabled = "#00000061";
+export const PaletteBackgroundDefault = "#FFFFFF";
+export const PaletteBackgroundPaper = "#FAFAFA";
+export const PaletteDivider = "#0000001f";
+export const PaletteActionSelected = "#2383e280";
+export const PaletteActionHover = "#F5F5F5";
+export const PaletteActionFocused = "#2383E2";
+export const PaletteActionDisabled = "#00000061";
+export const PaletteActionDisabledBackground = "#0000001f";
+export const TypographyH1 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":500,"lineHeight":32,"fontSize":28,"letterSpacing":0};
+export const TypographyH2 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"fontSize":24,"letterSpacing":0,"lineHeight":24};
+export const TypographyH3 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"fontSize":20,"letterSpacing":0,"lineHeight":20};
+export const TypographySubtitle1 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"letterSpacing":0,"lineHeight":20,"fontSize":16};
+export const TypographySubtitle2 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":500,"letterSpacing":0,"fontSize":12,"lineHeight":16};
+export const TypographyBody1 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"letterSpacing":0,"fontSize":16,"lineHeight":20};
+export const TypographyBody2 = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"letterSpacing":0,"fontSize":12,"lineHeight":16};
+export const TypographyButton = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":500,"letterSpacing":0,"fontSize":12,"lineHeight":16};
+export const TypographyCaption = {"fontFamily":"\"Roboto\",\"Helvetica\",\"Arial\",sans-serif","fontWeight":400,"letterSpacing":0,"fontSize":10.5,"lineHeight":12};
+export const BorderPrimary600 = {"color":"#757575"};
+export const BorderSecondary600 = {"color":"#1E88E5"};
+export const BorderSecondaryMain = {"color":"#2196F3"};
+export const BorderErrorMain = {"color":"#D32F2F"};
+export const BorderCheckbox = {"color":"#757575"};
+export const BorderTextPrimary = {"color":"#000000de"};
+export const BorderActionDisabled = {"color":"#00000061"};
+export const TokenSetOrder0 = "theme";
