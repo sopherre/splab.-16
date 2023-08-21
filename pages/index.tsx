00d550bc1b241fef078cdf2bf98b83986241630e
Shun E <shun-eto@sopherre.com>
2023-08-21 14:07:45 +0900

    ビルド修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 import type { NextPage } from "next";
-import { H1, H2 } from "../src/tokens/tokens";
+import * as tokens from "../src/tokens/data";
 
 const Home: NextPage = () => {
-  console.log(H1.fontWeight);
-
   return (
     <div>
-      <span style={{ fontFamily: H1.fontFamily, fontSize: H1.fontSize }}>
+      <span
+        style={{
+          fontFamily: tokens.TypographyH1.fontFamily,
+          fontSize: tokens.TypographyH1.fontSize,
+        }}
+      >
         h1
       </span>
-      <span style={{ fontFamily: H2.fontFamily, fontSize: H2.fontSize }}>
+      <span
+        style={{
+          fontFamily: tokens.TypographyH2.fontFamily,
+          fontSize: tokens.TypographyH2.fontSize,
+        }}
+      >
         h2
       </span>
     </div>
